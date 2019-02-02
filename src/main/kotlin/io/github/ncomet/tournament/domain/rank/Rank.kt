e437e30908023bab1aed23827da6b9a5e4b1cfd7
Nicolas Comet <nicolas.comet@gmail.com>
2019-02-02 11:37:42 +0100

    cleanup and add HealthCheck documentation

--- src/main/kotlin/io/github/ncomet/tournament/domain/rank/Rank.kt
+++ src/main/kotlin/io/github/ncomet/tournament/domain/rank/Rank.kt
@@ -1,7 +1,5 @@
 package io.github.ncomet.tournament.domain.rank
 
-import io.github.ncomet.tournament.domain.player.Player
-
 data class Rank(val value: Int, val outcome: Outcome) {
     init {
         require(value > 0) { "Rank.value must be strictly greater than 0" }
@@ -11,5 +9,3 @@ data class Rank(val value: Int, val outcome: Outcome) {
 enum class Outcome {
     TIE, UNTIE
 }
-
-typealias RankedPlayer = Pair<Player, Rank>
